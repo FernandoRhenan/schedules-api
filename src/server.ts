9fc0a1d03e3b610816ebc656055e5a296eefd2f7
FernandoRhenan <fernandorhenan9@gmail.com>
2023-06-11 15:48:07 -0300

    Primeira inteface

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express from 'express'
+import user_routes from './routes/Users'
 
 const server = express()
 
+server.use('/user', user_routes)
+
 
 const port = process.env.PORT || 3000
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log('Server rodando na porta ' + port)
 })
